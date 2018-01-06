96e0346c4e5003ca3965d555d89ad6ecee0a05aa
code34 <nicolas_boiteux@yahoo.fr>
2018-01-06 23:49:59 +0100

    first release

---
+++ Assets/scripts/UnCheckIsKinematicOnTriggerEnter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnCheckIsKinematicOnTriggerEnter : MonoBehaviour {
+
+	[SerializeField]
+	private Rigidbody _targetRigidbody;
+
+    [SerializeField]
+    private Vector3 _force;
+
+    public void OnDrawGizmos()
+	{
+		if (_targetRigidbody != null)
+			Debug.DrawLine(this.transform.position, _targetRigidbody.transform.position, Color.yellow);
+
+        Debug.DrawLine(this.transform.position, this.transform.position + _force / Physics.gravity.magnitude, Color.red);
+    }
+
+	public void OnTriggerEnter(Collider col)
+	{
+		if (_targetRigidbody != null)
+		{
+			_targetRigidbody.isKinematic = false;
+			_targetRigidbody.WakeUp();
+		}
+        if (col.GetComponent<Rigidbody>() != null)
+        {
+            col.GetComponent<Rigidbody>().AddForce(_force);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# plateforme
+A 3d plateforme test with Unity Engine
